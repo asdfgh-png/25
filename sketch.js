5822162cc72abb4fdcf58ab611ce2aa7869b960c
asdfgh-png <rakshitas115@gmail.com>
2021-04-10 17:55:43 +0530

    asd

--- sketch.js
+++ sketch.js
@@ -4,8 +4,9 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
+var pig3 ;
+var pig2 ;
 var backgroundImg,platform;
-
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,7 +34,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    pig2 = new Pig(50,50);
     bird = new Bird(100,100);
 
 }
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    pig2.display();
 }
